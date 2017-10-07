ab7d5f62114ea5fdf9278fd735b6dbb526d4b082
Min Wu <sunflash@users.noreply.github.com>
2017-10-07 21:41:07 +0200

    Add http client project.

---
+++ HTTPClient/HTTPClient/HTTPClient.h
@@ -0,0 +1,19 @@
+//
+//  HTTPClient.h
+//  HTTPClient
+//
+//  Created by Min Wu on 07/10/2017.
+//  Copyright © 2017 Min Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HTTPClient.
+FOUNDATION_EXPORT double HTTPClientVersionNumber;
+
+//! Project version string for HTTPClient.
+FOUNDATION_EXPORT const unsigned char HTTPClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTPClient/PublicHeader.h>
+
+
